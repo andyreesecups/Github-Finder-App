9bfaa90c144b8b579aeded220abcd975c3830d54
andyreesecups <andyreesecups@yahoo.com>
2017-09-09 13:31:12 -0500

    added panel that shows username

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-$(document).ready(function(){
-	$('#searchUser').on('keyup', function(e){
-		let username = e.target.value;
+$(document).ready(function() {
+    $('#searchUser').on('keyup', function(e) {
+        let username = e.target.value;
 
-		// https://api.github.com/users/andyreesecups
-		// Make request to Github API
-		$.ajax({
-			url: "https://api.github.com/users/"+username,
-			data: {
-				client_id: "b0e1ab1b9fdb44605e95",
-				client_secret: "1963fc958422fea65837caa040c1acb5f4ccdccc"
-			}
+        // https://api.github.com/users/andyreesecups
+        // Make request to Github API
+        $.ajax({
+            url: "https://api.github.com/users/" + username,
+            data: {
+                client_id: "b0e1ab1b9fdb44605e95",
+                client_secret: "1963fc958422fea65837caa040c1acb5f4ccdccc"
+            }
 
-		}).done();
-	});
+        }).done(function(user) {
+            // console.log(user);
+            $('#profile').html(`
+			<div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">${user.name}</h3>
+              </div>
+            <div class="panel-body">
+              Panel content
+              </div>
+            </div>
+        	`);
+        });
+    });
 });
