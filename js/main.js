dcbf0d893adc43ec263c7ab277042ec4be482f04
andyreesecups <andyreesecups@yahoo.com>
2017-09-14 18:23:31 -0500

    added latest repos

--- js/main.js
+++ js/main.js
@@ -5,14 +5,42 @@ $(document).ready(function() {
         // https://api.github.com/users/andyreesecups
         // Make request to Github API
         $.ajax({
-            url: "https://api.github.com/users/" + username,
+            url: 'https://api.github.com/users/' + username,
             data: {
-                client_id: "b0e1ab1b9fdb44605e95",
-                client_secret: "1963fc958422fea65837caa040c1acb5f4ccdccc"
+                client_id: 'b0e1ab1b9fdb44605e95',
+                client_secret: '1963fc958422fea65837caa040c1acb5f4ccdccc'
             }
 
         }).done(function(user) {
-            // console.log(user);
+            $.ajax({
+            	url: 'https://api.github.com/users/'+ username+'/repos',
+            	data: {
+                	client_id: 'b0e1ab1b9fdb44605e95',
+                	client_secret: '1963fc958422fea65837caa040c1acb5f4ccdccc',
+                	sort: 'created: asc',
+                	per_page: 5
+            	}
+            }).done(function(repos){
+            	$.each(repos, function(index, repo){
+            		$('#repos').append(`
+					<div class="well">
+						<div class="row">
+							<div class="col-md-7">
+								<strong>${repo.name}</strong>: ${repo.description}
+							</div>
+							<div class="col-md-3">
+								<span class="label label-default">Forks: ${repo.forks_count}</span>
+								<span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+								<span class="label label-success">Stars: ${repo.stargazers_count}</span>
+							</div>
+							<div class="col-md-2">
+								<a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+							</div>
+						</div>
+					</div>
+            		`);
+            	});
+            });
             $('#profile').html(`
 			<div class="panel panel-default">
               <div class="panel-heading">
@@ -30,10 +58,17 @@ $(document).ready(function() {
 				<span class="label label-success">Followers: ${user.followers}</span>
 				<span class="label label-info">Following: ${user.following}</span>
 				<br><br>
-			</div>
-              </div>
+				<ul class="list-group">
+				<li class="list-group-item">Portfolio/Blog: ${user.blog}</li>
+				<li class="list-group-item">Location: ${user.location}</li>
+				<li class="list-group-item">Member Since: ${user.created_at}</li>
+				</ul>
+				</div>
+              	</div>
               </div>
             </div>
+            <h3 class="page-header">Latest Repos</h3>
+            <div id="repos"></div>
         	`);
         });
     });
